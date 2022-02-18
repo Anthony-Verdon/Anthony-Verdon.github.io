8b20879c3a55694578599aecf5ce7fc50c888f2c
Anthony Verdon <anthonyverdon10@gmail.com>
2022-02-18 15:44:48 +0100

    envoie 1 portfolio

---
+++ portfolio/code.js
@@ -0,0 +1,59 @@
+const menu=document.getElementById("menu");
+
+function closemenu(){
+    menu.style.top ="-100vh";
+} 
+
+function openmenu(){
+    menu.style.top ="0";
+}
+
+let lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+    $('header').addClass('scrollVersBas');
+    $('texte').addClass('scrollVersBas');
+   } else {
+    $('header').removeClass('scrollVersBas');
+    $('texte').removeClass('scrollVersBas');
+   }
+   if (st > 0){
+    $('header').addClass('couleurScroll');
+    $('texte').addClass('couleurScroll');
+   } else {
+    $('header').removeClass('couleurScroll');
+    $('texte').removeClass('couleurScroll');
+   }
+   lastScrollTop = st;  
+});
+
+/** Ici la méthode addEventListener est appliquée à l'élément qui a pour ID 'survol' */
+document.getElementById('ImageGmail').addEventListener('mouseenter',
+/** cette méthode comprends la détection de l'événement ici onmouseenter pour un survol et une fonction qui va déterminenr l'action à effectuer lorsque l'événement est détecté*/
+function(){
+/* cette fonction indique de changer le style par l'attribut style de la balise concernnée- ici l'élément ayant pour id 'change'*/
+document.getElementById('texteGmail').setAttribute('style', 'color:white;opacity:1;position:absolute;top:17vw;font-size:1.5vw;left:36vw');
+
+}
+,false);
+
+document.getElementById('ImageGmail').addEventListener('mouseout',
+function(){
+    document.getElementById('texteGmail').setAttribute('style', 'color:white;opacity:0;position:absolute;top:17vw;font-size:1.5vw;left:36vw');
+}
+,false);
+
+
+document.getElementById('ImageDiscord').addEventListener('mouseenter',
+function(){
+document.getElementById('texteDiscord').setAttribute('style', 'color:white;opacity:1;position:absolute;top:17vw;font-size:1.5vw;left:49.5vw');
+}
+,false);
+
+document.getElementById('ImageDiscord').addEventListener('mouseout',
+function(){
+    document.getElementById('texteDiscord').setAttribute('style', 'color:white;opacity:0;position:absolute;top:17vw;font-size:1.5vw;left:36vw');
+}
+,false);
+
